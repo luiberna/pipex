19ec93c4b11d80fd7d2a3b91583f52ed4a587b0c
luiberna <146244524+luiberna@users.noreply.github.com>
2024-04-22 15:52:25 +0100

    Add files via upload

--- pipex.c
+++ pipex.c
@@ -6,38 +6,45 @@
 /*   By: luiberna <luiberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 12:11:26 by luiberna          #+#    #+#             */
-/*   Updated: 2024/04/16 18:03:14 by luiberna         ###   ########.fr       */
+/*   Updated: 2024/04/18 20:38:27 by luiberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	*get_path(char *argv, char **envp)
+void	free_list(char **list)
+{
+	int i;
+
+	i = 0;
+	while (list[i])
+		free(list[i++]);
+	free(list);
+}
+
+char	*get_path(char *cmd, char **envp)
 {
 	int i;
 	char *path;
-	char *part_path;
+	char *join_path;
 	char **paths;
 
 	i = 0;
-	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+	while (ft_strnstr(envp[i], "PATH=", 5) == 0)
 		i++;
 	paths = ft_split(envp[i] + 5, ':');
 	i = 0;
 	while (paths[i])
 	{
-		part_path = ft_strjoin(paths[i], "/");
-		path = ft_strjoin(part_path, argv);
-		free(part_path);
+		join_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(join_path, cmd);
+		free(join_path);
 		if (access(path, F_OK) == 0)
-			return (path);
-		free (path);
-		i++;
+			return (free_list(paths), path);
+		free(path);
+ 		i++;
 	}
-	i = 0;
-	while (paths[i])
-		free(paths[i++]);
-	return (free(paths), NULL);
+	return (free_list(paths), NULL);
 }
 void execute(char *argv, char **envp)
 {
@@ -47,18 +54,20 @@ void execute(char *argv, char **envp)
 
 	i = 0;
 	cmd = ft_split(argv, ' ');
-	path = get_path(cmd[0], envp);
-	if (!path)
+	if (!cmd[0])
 	{
-		while (cmd[i])
-			free(cmd[i++]);
-		free(cmd);
-		ft_printf("\033[31mError: invalid path\n\e[0m", 2);
-		exit(1);
+		free_list(cmd);
+		exit(write(2, "\033[31mError: No command\n\e[0m", 29));
+	}
+	path = get_path(cmd[0], envp);
+	if (path == NULL)
+	{
+		free_list(cmd);
+		exit(write(2, "\033[31mError: Invalid command\n\e[0m", 34));
 	}
 	if (execve(path, cmd, envp) == -1)
 	{
-		ft_printf("\033[31mError: Invalid command\n\e[0m", 2);
+		ft_printf("\033[31mError: Invalid command\n\e[0m");
 		exit(1);
 	}
 }
@@ -76,6 +85,8 @@ void	child(char **argv, char **envp, int *fd)
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(file, STDIN_FILENO);
 	close(fd[0]);
+	close(fd[1]);
+	close(file);
 	execute(argv[2], envp);
 }
 
@@ -92,30 +103,37 @@ void	parent(char **argv, char **envp, int *fd)
 	dup2(fd[0], STDIN_FILENO);
 	dup2(fileout, STDOUT_FILENO);
 	close(fd[1]);
+	close(fd[0]);
+	close(fileout);
 	execute(argv[3], envp);
+}
+
+void pipex(char **argv, char **envp)
+{
+	int fd[2];
+	pid_t pid;
+	
+	if (pipe(fd) == -1)
+		{
+			ft_printf("\033[31mError: Pipe not working, call Mario\n\e[0m", 2);
+			exit (1);
+		}
+		pid = fork();
+		if (pid == -1)
+			exit(ft_printf("\033[31mError: Invalid PID\n\e[0m", 2));
+		if (pid == 0)
+			child(argv, envp, fd);
+		else
+			parent(argv, envp, fd);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	int fd[2];
-	pid_t pid;
-
 	if (argc != 5)
 		return (ft_printf("\033[31mError: Not enough arguments\n\e[0m", 2), 0);
-	else 
-	{
-		if (pipe(fd) == -1)
-		{
-			ft_printf("\033[31mError: Pipe not working, call Mario\n\e[0m", 2);
-			return (0);
-		}
-		pid = fork();
-		if (pid == -1)
-			return (ft_printf("\033[31mError: Invalid PID\n\e[0m", 2), 0);
-		if (pid == 0)
-			child(argv, envp, fd);
-		waitpid(pid, NULL, 0);
-		parent(argv, envp, fd);
-	}
+	if (!envp[0])
+		return (ft_printf("\033[31mYou removed the env FY!\n\e[0m"));
+	else
+		pipex(argv, envp);
 	return (0);
 }
